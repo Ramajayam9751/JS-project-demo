8d6c4b1f04fab2778448d01eb8a297e5f297e448
Ramajayam9751 <vinosri162@gmail.com>
2026-04-29 22:38:42 +0530

    added

---
+++ A_funtionDeclaration.js
@@ -0,0 +1,21 @@
+
+
+function Message(){
+    return "welcome to credo system";
+}
+
+let MessageResult = message()
+
+
+
+/* Example 2 */
+
+function subractionCalculation(){
+    let a = 20;
+    let b = 30;
+    let c= a - b
+    return c;
+}
+
+
+let subractionResult = subractionCalculation()

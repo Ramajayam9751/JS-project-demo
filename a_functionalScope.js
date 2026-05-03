6fc043a438d578b91c2a8a135f2d1669bfa7c029
Ramajayam9751 <vinosri162@gmail.com>
2026-05-03 14:15:18 +0530

    added

---
+++ a_functionalScope.js
@@ -0,0 +1,9 @@
+var bottle = "oil"                           // declare
+var bottle = "water"                         // re-declare
+var bottle = "juice"
+var bottle = "curd"
+
+  bottle = "chilli powder"                // re-assign
+  bottle = "mouth-wash"
+
+console.log("bottle====>",bottle)
